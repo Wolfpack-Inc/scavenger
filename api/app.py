ee44cc30e7642a0092dcb006271e66bf2ad71775
Myrthe Wouters <myrthe@air-van-myrthe.home>
2019-10-19 15:59:07 +0200

    initial-suggestions

--- api/app.py
+++ api/app.py
@@ -31,5 +31,32 @@ class AllImage(Resource):
 
 api.add_resource(AllImage, '/images/all')
 
+class InitialSuggestion(Resource):
+    """
+    Get five closeby heritage images 
+    """
+    def get(self, long, lat):
+        subquery = (Locations
+                    .select(Locations.street.alias('street'), fn.sqrt(fn.pow(lat-Locations.dis_lat, 2) + 
+                                                              fn.pow(long-Locations.dis_long, 2))
+                            .alias('dist'),
+                            Locations.dis_long, Locations.dis_lat)
+                    .order_by(SQL('dist'))
+                    .limit(5)
+                    .alias('subquery'))
+        
+        images = (Images
+                  .select(fn.min(Images.id).alias('id'), Images.street, Images.url, Images.title, 
+                          subquery.c.dis_long, subquery.c.dis_lat)
+                  .join(subquery, on =
+                        (Images.street == subquery.c.street))
+                  .group_by(Images.street)
+                  .dicts()
+                  .execute())
+        
+        return [image for image in images]
+        
+api.add_resource(InitialSuggestion, '/images/initial/<float:long>/<float:lat>')
+          
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
